7942c949dd1613008a2f43aa18dfa1f5aab76472
yangxiaokai <1882120@163.com>
2021-05-26 20:13:50 +0800

    初始化

--- src/main/java/com/vueadmin/controller/TestController.java
+++ src/main/java/com/vueadmin/controller/TestController.java
@@ -3,17 +3,20 @@ package com.vueadmin.controller;
 import com.vueadmin.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
 
     @Autowired
     private SysUserService sysUserService;
 
-    @GetMapping("/list")
+    @RequestMapping("/list")
     public Object nihao() {
+        System.out.println("111");
         return sysUserService.list();
     }
 }
